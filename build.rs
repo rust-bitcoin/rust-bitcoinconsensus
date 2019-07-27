df33c1a75f701250a5aa2ec17dff295d0713e254
Jonas Nick <jonasd.nick@gmail.com>
2019-07-27 07:12:54 +0000

    Relax cc dependency requirements, bump secp256k1 to 0.15 and bump version to 0.17.0 (#9)
    * Relax cc dependency requirements, bump secp256k1 to 0.13 and bump version to 0.16.5
    
    * Update travis and appveyor to use minimum rustc version 1.22.0 required by secp256k1 0.13
    
    * Add WIN32 define for Windows target, remove endianness decls.
    
    * Relax cc requirements, bump secp256k1 to 0.15 and bump version to 0.17.0

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ extern crate cc;
 use std::env;
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
     let mut base_config = cc::Build::new();
     base_config
         .cpp(true)
@@ -19,22 +18,10 @@ fn main() {
     } else if tool.is_like_clang() || tool.is_like_gnu() {
         base_config.flag("-std=c++11").flag("-Wno-unused-parameter");
     }
-
-    if target == "x86_64-unknown-linux-gnu" {
+    #[cfg(target_os = "windows")]
+    {
         base_config
-            .define("HAVE_ENDIAN_H", Some("1"))
-            .define("HAVE_DECL_HTOBE16", Some("1"))
-            .define("HAVE_DECL_HTOLE16", Some("1"))
-            .define("HAVE_DECL_BE16TOH", Some("1"))
-            .define("HAVE_DECL_LE16TOH", Some("1"))
-            .define("HAVE_DECL_HTOBE32", Some("1"))
-            .define("HAVE_DECL_HTOLE32", Some("1"))
-            .define("HAVE_DECL_BE32TOH", Some("1"))
-            .define("HAVE_DECL_LE32TOH", Some("1"))
-            .define("HAVE_DECL_HTOBE64", Some("1"))
-            .define("HAVE_DECL_HTOLE64", Some("1"))
-            .define("HAVE_DECL_BE64TOH", Some("1"))
-            .define("HAVE_DECL_LE64TOH", Some("1"));
+            .define("WIN32", Some("1"));
     }
     base_config
         .file("bitcoin/src/utilstrencodings.cpp")
