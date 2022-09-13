9b3496a5c827e99cf10debe2c36f7b20fd622cc4
Tobin C. Harding <me@tobin.cc>
2022-09-13 14:52:46 +1000

    Remove libc dependency
    As we do in `rust-secp256k1` we can create type aliases for C integer
    types instead of depending on `libc`.
    
    Create type aliases (copied from `rust-secp256k1`) and remove libc
    dependency.
    
    Please note, this crate is copyright'ed to Tamas using Apache, but for
    this new file we use CC license since I copied it from
    `rust-secp256k1` (although its only a few trivial lines). Probably being
    overly paranoid but wanted to flag it.

---
+++ src/types.rs
@@ -0,0 +1,26 @@
+// Written by the Rust Bitcoin developers.
+// SPDX-License-Identifier: CC0-1.0
+
+#![allow(non_camel_case_types)]
+
+/// The C signed 32 bit integer type.
+pub type c_int = i32;
+/// The C unsigned 8 bit integer type.
+pub type c_uchar = u8;
+/// The C unsigned 32 bit integer type.
+pub type c_uint = u32;
+
+#[cfg(test)]
+mod tests {
+    use std::any::TypeId;
+    use std::os::raw;
+
+    use crate::types;
+
+    #[test]
+    fn verify_types() {
+        assert_eq!(TypeId::of::<types::c_int>(), TypeId::of::<raw::c_int>());
+        assert_eq!(TypeId::of::<types::c_uchar>(), TypeId::of::<raw::c_uchar>());
+        assert_eq!(TypeId::of::<types::c_uint>(), TypeId::of::<raw::c_uint>());
+    }
+}
